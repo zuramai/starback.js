c4fb0b47da3d0699df8405bafbb9d800886da1c3
Dicka Ismaji <dickaismaji@gmail.com>
2021-09-30 07:21:13 +0700

    Update: apply prettier

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,32 +1,32 @@
 const canvas = document.getElementById('canvas')
-const starback = new Starback(canvas,{
-    width: canvas.parentElement.clientWidth,
-    height: canvas.parentElement.clientHeight,
-    speed: 5,
-    frequency: 5,
-    slope: {x: 5, y:3},
-    directionX: 1,
-    spread: .2,
-    randomOpacity: true,
-    backgroundColor: ["#0F2027", "#203A43", "#2C5364"]
+const starback = new Starback(canvas, {
+  width: canvas.parentElement.clientWidth,
+  height: canvas.parentElement.clientHeight,
+  speed: 5,
+  frequency: 5,
+  slope: { x: 5, y: 3 },
+  directionX: 1,
+  spread: 0.2,
+  randomOpacity: true,
+  backgroundColor: ['#0F2027', '#203A43', '#2C5364'],
 })
 
 starback.generateStar(20)
 let mountain = new Image()
-mountain.src = "images/mountain2.png"
+mountain.src = 'images/mountain2.png'
 
 mountain.onload = () => {
-    starback.addToFront(ctx => {
-        ctx.drawImage(
-            mountain,
-            0,
-            0,
-            mountain.width,
-            mountain.height,
-            0,
-            canvas.height - mountain.height,
-            canvas.width,
-            mountain.height
-            )
-    })
+  starback.addToFront((ctx) => {
+    ctx.drawImage(
+      mountain,
+      0,
+      0,
+      mountain.width,
+      mountain.height,
+      0,
+      canvas.height - mountain.height,
+      canvas.width,
+      mountain.height
+    )
+  })
 }
